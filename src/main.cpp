435ba96fbd7fad5c1d3ca7312f03dd72f2b2a794
trauty <trautwein.maximilian04@gmail.com>
2025-12-23 03:57:17 +0100

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "test_component.h"
 
-#include <smol/asset/asset.h>
-#include <smol/asset/asset_manager.h>
+#include <smol/asset.h>
 #include <smol/asset/mesh.h>
 #include <smol/asset/shader.h>
 #include <smol/asset/texture.h>
@@ -12,9 +11,6 @@
 #include <smol/engine.h>
 #include <smol/rendering/spatial_material.h>
 
-using namespace smol::asset_manager;
-using namespace smol::asset;
-
 int main()
 {
     smol::engine::init("smol-game", 1280, 720);
@@ -23,31 +19,32 @@ int main()
     smol::engine::load_level(first_level);
     std::shared_ptr<smol::core::gameobject_t> cam_obj = first_level->create_gameobject("cam");
     cam_obj->add_component<smol::components::camera_ct>()->set_as_main_camera();
-    smol::math::vec3_t new_rot {0.0f, 0.0f, 0.0f};
+    smol::math::vec3_t new_rot{0.0f, 0.0f, 0.0f};
     cam_obj->get_transform()->rotate_local(new_rot);
 
     std::shared_ptr<smol::core::gameobject_t> cube_obj = first_level->create_gameobject("cube");
     smol::components::mesh_renderer_ct* mesh_renderer = cube_obj->add_component<smol::components::mesh_renderer_ct>();
     cube_obj->add_component<smol_game::test_component_t>();
 
-    asset_handle_t shader_handle = smol::asset_manager::load_sync<shader_asset_t>("assets/shaders/unlit.vert|assets/shaders/unlit.frag");
-    asset_ptr_t<shader_asset_t> shader_prog = smol::asset_manager::get<shader_asset_t>(shader_handle);
+    smol::asset_t<smol::shader_asset_t> shader_handle =
+        smol::asset_manager_t::load<smol::shader_asset_t>("assets/shaders/unlit.slang");
 
-    asset_handle_t model_handle = smol::asset_manager::load_sync<mesh_asset_t>("assets/models/croissant.glb");
-    asset_ptr_t<mesh_asset_t> model = smol::asset_manager::get<mesh_asset_t>(model_handle);
+    smol::asset_t<smol::mesh_asset_t> model_handle =
+        smol::asset_manager_t::load<smol::mesh_asset_t>("assets/models/croissant.glb");
 
-    asset_handle_t tex_handle = smol::asset_manager::load_sync<texture_asset_t>("assets/textures/pastry.png", texture_asset_args_t {texture_type_e::ALBEDO});
-    asset_ptr_t<texture_asset_t> rock_tex = smol::asset_manager::get<texture_asset_t>(tex_handle);
+    smol::asset_t<smol::texture_asset_t> tex_handle =
+        smol::asset_manager_t::load<smol::texture_asset_t>("assets/textures/pastry.png", smol::texture_type_e::ALBEDO);
 
-    std::shared_ptr<smol::rendering::spatial_material_t> cube_mat = std::make_shared<smol::rendering::spatial_material_t>(shader_prog);
-    cube_mat->albedo_texture = rock_tex;
-    mesh_renderer->set_mesh(model);
+    std::shared_ptr<smol::rendering::spatial_material_t> cube_mat =
+        std::make_shared<smol::rendering::spatial_material_t>(shader_handle);
+    cube_mat->albedo_texture = tex_handle;
+    mesh_renderer->set_mesh(model_handle);
     mesh_renderer->set_material(cube_mat);
 
-    smol::math::vec3_t new_pos {0.0f, 0.0f, 5.0f};
+    smol::math::vec3_t new_pos{0.0f, 0.0f, 5.0f};
     cube_obj->get_transform()->set_local_position(new_pos);
 
-    smol::math::vec3_t new_scale {20.0f, 20.0f, 20.0f};
+    smol::math::vec3_t new_scale{20.0f, 20.0f, 20.0f};
     cube_obj->get_transform()->set_local_scale(new_scale);
 
     smol::engine::run();
--- src/test_component.cpp
+++ src/test_component.cpp
@@ -4,8 +4,7 @@
 #include "smol/time_util.h"
 
 #include <cmath>
-#include <smol/asset/asset.h>
-#include <smol/asset/asset_manager.h>
+#include <smol/asset.h>
 #include <smol/asset/mesh.h>
 #include <smol/asset/shader.h>
 #include <smol/asset/texture.h>
@@ -14,9 +13,6 @@
 #include <smol/log.h>
 #include <smol/math_util.h>
 
-using namespace smol::asset_manager;
-using namespace smol::asset;
-
 #include <iostream>
 
 namespace smol_game
@@ -24,20 +20,22 @@ namespace smol_game
     void test_component_t::start()
     {
         /*SMOL_LOG_INFO("TEST_COMPONENT", "Start wurde aufgerufen");
-        asset_handle_t handle = asset_manager::load_sync<texture_asset_t>("assets/uiia_cat.png", texture_asset_args_t { texture_type_e::BASE_COLOR });
-        asset_ptr_t<texture_asset_t> image = asset_manager::get<texture_asset_t>(handle);
+        asset_handle_t handle = asset_manager::load_sync<texture_asset_t>("assets/uiia_cat.png", texture_asset_args_t {
+        texture_type_e::BASE_COLOR }); asset_ptr_t<texture_asset_t> image = asset_manager::get<texture_asset_t>(handle);
         SMOL_LOG_INFO("TEST_COMPONENT", "Breite: {}; Höhe: {};", image->get_width(), image->get_height());
-        
+
         asset_manager::load_async<texture_asset_t>(
-            "assets/uiia_cat.png", 
-            texture_asset_args_t { texture_type_e::BASE_COLOR }, 
+            "assets/uiia_cat.png",
+            texture_asset_args_t { texture_type_e::BASE_COLOR },
             [](asset_handle_t async_handle) {
                 asset_ptr_t<texture_asset_t> async_image = asset_manager::get<texture_asset_t>(async_handle);
-                SMOL_LOG_INFO("TEST_COMPONENT", "Breite: {}; Höhe: {};", async_image->get_width(), async_image->get_height());
+                SMOL_LOG_INFO("TEST_COMPONENT", "Breite: {}; Höhe: {};", async_image->get_width(),
+        async_image->get_height());
             }
         );
 
-        asset_handle_t shader_handle = asset_manager::load_sync<shader_asset_t>("assets/shaders/default.vert|assets/shaders/default.frag");
+        asset_handle_t shader_handle =
+        asset_manager::load_sync<shader_asset_t>("assets/shaders/default.vert|assets/shaders/default.frag");
         asset_ptr_t<shader_asset_t> shader_prog = asset_manager::get<shader_asset_t>(shader_handle);
 
         asset_handle_t model_handle = asset_manager::load_sync<mesh_asset_t>("assets/models/cube.glb");
@@ -50,7 +48,9 @@ namespace smol_game
         vec3_t rot = {30.0f * (f32)delta_time, 20.0f * (f32)delta_time, 40.0f * (f32)delta_time};
         get_gameobject()->get_transform()->rotate_local(rot);
 
-        vec3_t scale = {30.0f * std::abs(std::sin((f32)smol::time::get_time_in_seconds())), 30.0f * std::abs(std::sin((f32)smol::time::get_time_in_seconds())), 30.0f * std::abs(std::sin((f32)smol::time::get_time_in_seconds()))};
+        vec3_t scale = {30.0f * std::abs(std::sin((f32)smol::time::get_time_in_seconds())),
+                        30.0f * std::abs(std::sin((f32)smol::time::get_time_in_seconds())),
+                        30.0f * std::abs(std::sin((f32)smol::time::get_time_in_seconds()))};
         get_gameobject()->get_transform()->set_local_scale(scale);
     }
 } // namespace smol_game
