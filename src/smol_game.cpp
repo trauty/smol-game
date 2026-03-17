497f0993515251221ef3e9d486058fd7031e12cd
trauty <trautwein.maximilian04@gmail.com>
2026-03-17 17:07:19 +0100

    changed game test

--- src/smol_game.cpp
+++ src/smol_game.cpp
@@ -21,47 +21,78 @@ extern "C"
 {
     SMOL_API void smol_game_init(smol::world_t* world)
     {
-        asset_t<mesh_t> cube_mesh = smol::load_asset_sync<mesh_t>("assets/models/croissant.glb");
-        asset_t<texture_t> cube_tex = smol::load_asset_sync<texture_t>("assets/textures/low_quality_pastry.png");
-        asset_t<shader_t> cube_shader = smol::load_asset_sync<shader_t>("assets/shaders/unlit.slang");
-        asset_t<material_t> cube_material = smol::load_asset_sync<material_t>("cube_material", cube_shader);
+        asset_t<mesh_t> croissant_mesh = smol::load_asset_sync<mesh_t>("assets/models/croissant.glb");
+        asset_t<mesh_t> monkee_mesh = smol::load_asset_sync<mesh_t>("assets/models/monkee.glb");
+        asset_t<texture_t> croissant_tex = smol::load_asset_sync<texture_t>("assets/textures/low_quality_pastry.png");
+        asset_t<shader_t> unlit_shader = smol::load_asset_sync<shader_t>("assets/shaders/unlit.slang");
+        asset_t<material_t> croissant_material = smol::load_asset_sync<material_t>("cube_material", unlit_shader);
 
-        cube_material->set_property<color_t>("color", color_t());
-        cube_material->set_texture("albedo_tex", cube_tex);
-        cube_material->set_sampler("albedo_sampler", sampler_type_e::NEAREST_REPEAT);
+        asset_t<texture_t> rock_tex = smol::load_asset_sync<texture_t>("assets/textures/rock_albedo.png");
+
+        std::vector<asset_t<material_t>> material_palette;
+
+        color_t colors[5] = {
+            {1.0f, 1.0f, 1.0f, 1.0f},
+            {1.0f, 0.2f, 0.2f, 1.0f},
+            {0.2f, 1.0f, 0.2f, 1.0f},
+            {0.2f, 0.2f, 1.0f, 1.0f},
+            {1.0f, 1.0f, 0.2f, 1.0f}
+        };
+
+        for (int i = 0; i < 5; i++)
+        {
+            std::string mat_name = "croissant_mat_" + std::to_string(i);
+            asset_t<material_t> mat = smol::load_asset_sync<material_t>(mat_name.c_str(), unlit_shader);
+
+            mat->set_property<color_t>("color", colors[i]);
+            mat->set_texture("albedo_tex", i % 2 == 0 ? croissant_tex : rock_tex);
+            mat->set_sampler("albedo_sampler", sampler_type_e::NEAREST_REPEAT);
+
+            material_palette.push_back(mat);
+        }
 
         ecs::registry_t& reg = world->registry;
 
-        ecs::entity_t test_entity = reg.create();
-        reg.emplace<rotator_t>(test_entity);
-        mesh_renderer_t& renderer = reg.emplace<mesh_renderer_t>(test_entity);
-        renderer.mesh = cube_mesh;
-        renderer.material = cube_material;
+        int spawn_count = 0;
+        for (int x = -10; x < 10; x++)
+        {
+            for (int y = -10; y < 10; y++)
+            {
+                ecs::entity_t test_entity = reg.create();
+                reg.emplace<rotator_t>(test_entity);
+                mesh_renderer_t& renderer = reg.emplace<mesh_renderer_t>(test_entity);
 
-        reg.emplace<transform_t>(test_entity);
+                renderer.mesh = spawn_count % 2 == 0 ? croissant_mesh : monkee_mesh;
+
+                renderer.material = material_palette[spawn_count % material_palette.size()];
+
+                reg.emplace<transform_t>(test_entity);
+                smol::transform_system::set_local_position(reg, test_entity, {x * 2.0f, y * 2.0f, 0.0f});
+
+                if (renderer.mesh != monkee_mesh)
+                {
+                    smol::transform_system::set_local_scale(reg, test_entity, {15.0f, 15.0f, 15.0f});
+                }
+
+                spawn_count++;
+            }
+        }
 
         ecs::entity_t cam_entity = reg.create();
         reg.emplace<camera_t>(cam_entity);
         reg.emplace<transform_t>(cam_entity);
         reg.emplace<active_camera_tag>(cam_entity);
 
-        smol::transform_system::set_local_position(reg, cam_entity, {0.0f, 0.0f, -5.0f});
-        smol::transform_system::set_local_position(reg, test_entity, {0.0f, 0.0f, 0.0f});
+        smol::transform_system::set_local_position(reg, cam_entity, {0.0f, 0.0f, -36.0f});
 
         smol::engine::get_active_world().register_update_system(
             [](ecs::registry_t& reg)
             {
                 vec3_t rot = {0.8f * (f32)time::get_time(), 0.7f * (f32)time::get_time(), 0.9f * (f32)time::get_time()};
-                /*vec3_t scale = {30.0f * std::abs(std::sin((f32)time::time)),
-                                30.0f * std::abs(std::sin((f32)time::time)),
-                                30.0f * std::abs(std::sin((f32)time::time))};*/
-
-                vec3_t scale = {15.0f, 15.0f, 15.0f};
 
                 for (auto [entity, rotator, transform] : reg.view<rotator_t, transform_t>().each())
                 {
                     transform.local_rotation = quat_t::from_euler(rot);
-                    transform.local_scale = scale;
                     transform.is_dirty = true;
                 }
             });
