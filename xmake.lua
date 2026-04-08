26eaf8bb5ff74c08ea56a6f22c06896908117226
trauty <trautwein.maximilian04@gmail.com>
2026-04-08 04:06:21 +0200

    added android build and sdl android template

--- xmake.lua
+++ xmake.lua
@@ -27,6 +27,9 @@ local is_standalone = has_config("standalone")
 target("smol-game")
     if is_standalone then
         set_kind("static")
+        if is_plat("android") then
+            add_cxflags("-fPIC", {force = true})
+        end
     else
         set_kind("shared")
         if is_plat("linux") then 
@@ -35,12 +38,17 @@ target("smol-game")
     end
     
     set_basename(get_config("game_lib_name"))
-    add_cxflags("-march=x86-64-v3")
+    
+    if is_arch("x86_64", "x64") then
+        add_cxflags("-march=x86-64-v3")
+    end
 
     set_targetdir("bin")
 
     if is_mode("debug") then
-        set_policy("build.sanitizer.address", true)
+        if not is_plat("android") then
+            set_policy("build.sanitizer.address", true)
+        end
 
         if is_plat("windows") then
             add_defines("_DISABLE_STRING_ANNOTATION", "_DISABLE_VECTOR_ANNOTATION", {public = true})
@@ -55,34 +63,36 @@ target("smol-game")
     end
     
     add_deps("smol-interface")
-    add_deps("smol-cooker", {inherit = false})
     add_deps("smol-engine")
 
     add_files("src/**.cpp")
     add_includedirs("src")
 
-    after_build(function (target)
-        local dest_dir = target:targetdir()
+    if not is_plat("android") then
+        add_deps("smol-cooker", {inherit = false})
+        after_build(function (target)
+            local dest_dir = target:targetdir()
 
-        local cooker_bin = target:dep("smol-cooker"):targetfile()
-        local engine_assets = path.join(os.scriptdir(), "smol-engine/assets")
-        local game_assets = "assets"
-        local out_dir = ".smol"
+            local cooker_bin = target:dep("smol-cooker"):targetfile()
+            local engine_assets = path.join(os.scriptdir(), "smol-engine/assets")
+            local game_assets = "assets"
+            local out_dir = ".smol"
 
-        print("Running smol-cooker...")
-        os.execv(cooker_bin, {
-            "-i", engine_assets,
-            "-i", game_assets,
-            "-o", out_dir
-        })
+            print("Running smol-cooker...")
+            os.execv(cooker_bin, {
+                "-i", engine_assets,
+                "-i", game_assets,
+                "-o", out_dir
+            })
 
-        local target_assets = path.join(target:dep("smol-cooker"):targetdir(), out_dir)
-        os.mkdir(target_assets)
-        os.cp(path.join(out_dir, "*"), target_assets)
+            local target_assets = path.join(target:dep("smol-cooker"):targetdir(), out_dir)
+            os.mkdir(target_assets)
+            os.cp(path.join(out_dir, "*"), target_assets)
 
-        print("Cooked assets and copied folder to build dir: " .. target_assets)
+            print("Cooked assets and copied folder to build dir: " .. target_assets)
 
-        local trigger_file = target:targetfile() .. ".trigger"
-        io.writefile(trigger_file, os.date("%Y-%m-%d %H:%M:%S"))
-    end)
+            local trigger_file = target:targetfile() .. ".trigger"
+            io.writefile(trigger_file, os.date("%Y-%m-%d %H:%M:%S"))
+        end)
+    end
 target_end()
